7051311468650202669dd044eec60f734962267f
Odair Jose Santos Junior <odair@odairsantos.com.br>
2021-07-18 14:08:17 +0000

    Versão inicial

---
+++ webapp/test/integration/AllJourneys.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+	"sap/ui/test/Opa5",
+	"./arrangements/Startup",
+	"./WorklistJourney",
+	"./NavigationJourney",
+	"./NotFoundJourney",
+	"./ObjectJourney"
+], function (Opa5, Startup) {
+	"use strict";
+
+	Opa5.extendConfig({
+		arrangements: new Startup(),
+		viewNamespace: "ovly.fiori61.worklistplantas.view.",
+		autoWait: true
+	});
+
+});
